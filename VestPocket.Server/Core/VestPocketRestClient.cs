d2600b5a78924a2eb05935cd7e380a42575ea120
Scarlet Rose <70824102+scarletquasar@users.noreply.github.com>
2024-01-13 03:11:33 -0300

    chore(wip): base impl for VestPocketRestClient

---
+++ VestPocket.Server/Core/VestPocketRestClient.cs
@@ -0,0 +1,35 @@
+﻿using VestPocket.ClientServer.Interfaces;
+
+namespace VestPocket.ClientServer.Core
+{
+    public class VestPocketRestClient : IVestPocketClient
+    {
+        public IRemoteVestPocketStore<TEntity> Open<TEntity>(string store, CancellationToken ct = default)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    public class RemoteVestPocketRestStore<T> : IRemoteVestPocketStore<T>
+    {
+        public Task Close(CancellationToken ct = default)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<T> Get(string key, CancellationToken ct = default)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<T> GetByPrefix(string prefix, CancellationToken ct = default)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<T> Save(T entity, CancellationToken ct = default)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
